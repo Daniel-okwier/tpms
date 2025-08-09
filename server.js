4fe81dd751b2b472d6d027ea0ac2e6b00232a1d0
Daniel-okwier <coderdani229@gmail.com>
2025-08-09 18:31:15 +0300

    Create auth-routes

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import connectDB from './config/db.js'; 
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
-
-// Connect to MongoDB
 connectDB();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes placeholder
+// Routes
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
   res.send('TPMS Backend API is running...');
 });
 
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-  console.log(` Server running on port ${PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
