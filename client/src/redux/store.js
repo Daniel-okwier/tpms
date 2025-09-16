0d4d3a9b2a80d78273b03343703f4f304c3ef651
Daniel-okwier <coderdani229@gmail.com>
2025-09-16 14:34:59 +0300

    add labTests slice to Redux store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
-import userReducer from "./slices/userSlice"; 
+import userReducer from "./slices/userSlice";
 import patientReducer from "./slices/patientSlice";
+import labTestsReducer from "./slices/labTestsSlice"; 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     users: userReducer,
-    patients:patientReducer
-     
+    patients: patientReducer,
+    labTests: labTestsReducer, 
   },
 });
 
